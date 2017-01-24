12b4ed15c52740cc97b3f1b462a5ee66d1fc39f3
Dylan Hicks <dman1366@gmail.com>
2017-01-23 22:46:57 -0700

    Clean main of useless code.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,2 @@
-extern crate libterminal_cribbage;
-
-
-use libterminal_cribbage::cards::Deck;
-
-
 fn main() {
-    let mut d = Deck::new();
-
-    for _ in 0..100000 {
-        d = Deck::new();
-    }
 }
